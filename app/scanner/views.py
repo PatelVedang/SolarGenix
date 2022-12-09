6a520835e77d951829bd4c9a62dc85c6cdd34a32
ugam.thakkar <ugam.thakkar@isaix.com>
2022-12-09 11:39:20 +0530

    2533-Get-status-of-all-pending-tasks

--- app/scanner/views.py
+++ app/scanner/views.py
@@ -16,26 +16,61 @@ class ScanViewSet(viewsets.ModelViewSet):
     # API for create object
     def create(self, request, *args, **kwargs):
         # provide payload to serializer
-        payload = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
-        #validate serializer with payload 
-        if payload.is_valid(raise_exception=True):
+        #validate serializer with given payload 
+        if serializer.is_valid(raise_exception=True):
             ip = request.data['ip']
             client = request.data['client']
             
             # Create record if it is not exist with provided ip and client  
             record = Machine.objects.filter(ip=ip, client=client)
             if not record.exists():
-                payload.save()
+                serializer.save()
             else:
                 record.update(bg_task_status=True)
             # run background function 
             scan.delay(ip, client)
-        return response(data = payload.data, status_code = status.HTTP_200_OK, message="host successfully added in queue")
+        return response(data = serializer.data, status_code = status.HTTP_200_OK, message="host successfully added in queue")
 
     # API to retrive any scaned host
     def retrieve(self, request, *args, **kwargs):
         self.serializer_class = ScannerResponseSerializer
-        payload = super().retrieve(request, *args, **kwargs)
-        print(payload,"=>>>retyriev")
-        return response(data = payload.data, status_code = status.HTTP_200_OK, message="record found successfully")
+        serializer = super().retrieve(request, *args, **kwargs)
+        return response(data = serializer.data, status_code = status.HTTP_200_OK, message="record found successfully")
+
+    # API to get list of scan records
+    # Query Params
+    # -client(client id), ip(ip in string), scanned(0 or 1), bg_task_status(0 or 1)
+    def list(self, request, *args, **kwargs):
+        result = Machine.objects.all()
+        params = dict(request.query_params)
+        if params.get('client'):
+            result = result.filter(client=params.get('client')[0])
+        if params.get('ip'):
+            result = result.filter(ip=params.get('ip')[0])
+
+        try:
+            if params.get('scanned'):
+                if params.get('scanned')[0] in ['0','1']:
+                    scanned = bool(int(params.get('scanned')[0]))
+                    result = result.filter(scanned=scanned)
+        except Exception as e:
+            pass
+        
+        try:
+            if params.get('bg_task_status'):
+                if params.get('bg_task_status')[0] in ['0','1']:
+                    bg_task_status = bool(int(params.get('bg_task_status')[0]))
+                    result = result.filter(bg_task_status=bg_task_status)
+        except Exception as e:
+            pass
+
+        message = "record found successfully"
+        if not result:
+            message = "record not found"
+    
+        self.serializer_class = ScannerResponseSerializer
+        serializer = self.serializer_class(result, many=True)
+        return response(data = serializer.data, status_code = status.HTTP_200_OK, message=message)
+    
