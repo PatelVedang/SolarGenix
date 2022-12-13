6d0c41d0fbaabf3a779f4e5c7d6f4d5c84d1dbb2
ugam.thakkar <ugam.thakkar@isaix.com>
2022-12-13 17:34:39 +0530

    2541-Develop-time-based-task-termination

--- app/scanner/tasks.py
+++ app/scanner/tasks.py
@@ -11,25 +11,28 @@ from celery.exceptions import SoftTimeLimitExceeded
 @shared_task(time_limit=40)
 def scan(ip, client):
     start_time = time.time()
-    result = ""
-    # output = subprocess.getoutput(f'nmap -Pn -sV {self.IPAddress} -p23,3389,445')
-    output = subprocess.getoutput(f'nmap -Pn -sV {ip} --host-timeout 30s')
-    search_regex = '(?P<port>\d{1,4}/tcp)\s+(?P<state>(filtered|open|closed))'
-    header_regex = 'PORT\s+STATE'
-    ports = list(re.finditer(search_regex, output))
-    headers = re.search(header_regex, output)
+    output = ""
     machine = Machine.objects.filter(ip=ip, client=client)
-    
-    # If Scan thread is Terminated after 30s
-    if not headers:
+
+    try:
+        # output = subprocess.check_output(f"nmap -Pn -sV {ip} -p23,3389,445", shell=True, timeout=30).decode('utf-8')
+        output = subprocess.check_output(f"nmap -Pn -sV {ip}", shell=True, timeout=30).decode('utf-8')
+    except Exception as e:
         print("====>>>>>>>>       ", "Background Thread Terminated", "       <<<<<<<<====")
         machine.update(result=output, scanned=False, bg_task_status=True)
-        end_time = time.time()
-        result += f"{fg('blue_3b')}Scan completed in {round(end_time-start_time)}s{attr('reset')}\n\n{fg('white')}{attr('bold')}Vulnerability Threat Level{attr('reset')}"
-        return result
+        return False
+
+    port_search_regex = '(?P<port>\d{1,4}/tcp)\s+(?P<state>(filtered|open|closed))'
+    ignore_state_regex = "All 1000 scanned ports on \d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} are in ignored states."
+    ports = list(re.finditer(port_search_regex, output))
     
-    machine.update(result=output, scanned=True, bg_task_status=True)
+    # If not found any open ports
+    if re.search(ignore_state_regex, output):
+        machine.update(result=output, scanned=True, bg_task_status=True)
+    
+    # If ports found in given time
+    elif ports:
+        machine.update(result=output, scanned=True, bg_task_status=True)
     end_time = time.time()
-    result += f"{fg('blue_3b')}Scan completed in {round(end_time-start_time)}s{attr('reset')}\n\n{fg('white')}{attr('bold')}Vulnerability Threat Level{attr('reset')}"
-    return result
+    return True
     
