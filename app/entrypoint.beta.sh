e0f92ed37aa5788ceb2641d46cdede642f327353
ugam.thakkar <ugam.thakkar@isaix.com>
2023-01-02 18:14:46 +0530

    2581-Fix-execution-error-of-entrypoint.sh-on-windows

--- app/entrypoint.beta.sh
+++ app/entrypoint.beta.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "mysql" ]
-then
-    echo "Waiting for Mysql..."
-
-    while ! nc -z $MYSQL_DATABASE_HOST $MYSQL_DATABASE_PORT; do
-      sleep 0.1
-    done
-fi
-
 # python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata scanner/fixtures/superuser.json --app scanner.user
