73fd2d28504d7068e66e37d0d9d108a4d066a824
ugam.thakkar <ugam.thakkar@isaix.com>
2023-01-23 16:01:21 +0530

    2632-Add-unit-tests-for-Scan-API

--- app/scanner/tests.py
+++ app/scanner/tests.py
@@ -41,3 +41,86 @@ class ExceptionHandlerTest(BaseAPITestCase):
         }
         self.set_response(self.client.post(f'{self.prefix}scan/', data=self._data, format = "json"))
         self.match_error_response()
+
+# It tests the API endpoints of the Tool app.
+class ScanTest(BaseAPITestCase):
+    prefix = '/api/'
+
+    def insert_records(self):
+        """
+        It creates a tool record, then creates a scan record with the tool_id and two ip addresses
+        """
+        self._data = {
+            "tool_name": "nmap",
+            "tool_cmd": "nmap -Pn -sV"
+        }
+        self.set_response(self.client.post(f'{self.prefix}tool/', data=self._data , format = "json"))
+        tool_id = self._data.get('data').get('id')
+        self._data = {
+            "ip": [
+                "20.220.195.124","20.220.195.124"
+            ],
+            "tools_id": [
+                tool_id
+            ]
+        }
+        self.set_response(self.client.post(f'{self.prefix}scan/', data=self._data , format = "json"))
+    
+    def test_list_api(self):
+        """
+        It tests the list API of the scan app
+        """
+        self.login()
+        self.insert_records()
+        self.set_response(self.client.get(f'{self.prefix}scan/', format = "json"))
+        self.match_success_response()
+       
+
+    def test_create_api(self):
+        """
+        It creates some machine records, and then matches the response
+        """
+        self.login()
+        self.insert_records()
+        self.match_success_response()
+    
+    def test_add_by_ids_api(self):
+        """
+        It logs in, inserts records, gets the first record, sets the data, sets the response, and
+        matches the success response
+        """
+        self.login()
+        self.insert_records()
+        obj = Machine.objects.first()
+        self._data = {
+            "machines_id": [
+                obj.id
+            ]
+        }
+        self.set_response(self.client.post(f'{self.prefix}scan/addByIds/', data=self._data , format = "json"))
+        self.match_success_response()
+    
+    def test_add_by_numbers_api(self):
+        """
+        It tests the API endpoint `/scan/addByNumbers/` by sending a POST request with the data
+        `{"count": 5}` and checks if the response is successful
+        """
+        self.login()
+        self.insert_records()
+        self._data = {
+            "count": 5
+        }
+        self.set_response(self.client.post(f'{self.prefix}scan/addByNumbers/', data=self._data , format = "json"))
+        self.match_success_response()
+    
+    def test_retrieve_api(self):
+        """
+        It tests the retrieve API endpoint for the Machine app
+        """
+        self.login()
+        self.insert_records()
+        obj = Machine.objects.first()
+        self.set_response(self.client.get(f'{self.prefix}scan/{obj.id}/', format = "json"))
+        self.match_success_response()
+
+    
