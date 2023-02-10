d90ac0dca6e4cc8a5cc5f595ac0cc61b5b38b2b0
ugam.thakkar <ugam.thakkar@isaix.com>
2023-02-10 18:00:51 +0530

    2676-Add-sorting,-searching,-and-filtering-features-on-the-back-end

--- app/proj/settings.py
+++ app/proj/settings.py
@@ -166,24 +166,9 @@ SWAGGER_SETTINGS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://localhost:3000",
-    "http://localhost"
-
-]
-
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000",
-#     "http://localhost:3000",
-#     "http://localhost"
-# ]
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8000",
-    "http://localhost:3000",
-    "http://localhost"
-]
-
+CSRF_TRUSTED_ORIGINS=os.environ.get('CSRF_TRUSTED_ORIGINS', env('CSRF_TRUSTED_ORIGINS')).split()
+CORS_ORIGIN_WHITELIST=os.environ.get('CSRF_TRUSTED_ORIGINS', env('CSRF_TRUSTED_ORIGINS')).split()
+    
 
 # CORS_ORIGIN_ALLOW_ALL = True
 
