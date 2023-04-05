d774913114ed86c4082cadc4a709f9f901ccef93
ugam.thakkar <ugam.thakkar@isaix.com>
2023-04-05 18:38:20 +0530

    2731-Support-for-NIKTO

--- app/utils/handlers/nikto_handler.py
+++ app/utils/handlers/nikto_handler.py
@@ -12,7 +12,7 @@ class NIKTO:
 
     def nikto_handler(self, target, regenerate):
         if regenerate or target.compose_result=="":
-            
+            self.result = ""
             if re.search(self.regex, target.raw_result, re.IGNORECASE):
                 self.result ='''
                 <div class="col-12 border border-1 border-dark" style="background-color:orange;">
@@ -25,7 +25,10 @@ class NIKTO:
                 </div>
                 '''
             else:
-                self.result += f'''
+                self.result = f'''
+                <div class="col-12 border border-1 border-dark">
+                        <h2>Nikto.</h2>
+                </div>
                 <div class="col-12 border border-1 border-dark">
                         <h3>No Vulnerability Threat Level Found</h3>
                 </div>'''
