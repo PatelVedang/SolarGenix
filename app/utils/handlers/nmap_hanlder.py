4567709beb52f89f93b51532a7f573902a356aa0
ugam.thakkar <ugam.thakkar@isaix.com>
2023-05-01 14:10:19 +0530

    2764-Add-support-for-UDP-port

--- app/utils/handlers/nmap_hanlder.py
+++ app/utils/handlers/nmap_hanlder.py
@@ -38,7 +38,6 @@ class NMAP:
         }
         tool_cmd = target.tool.tool_cmd.strip()
         if handlers.get(tool_cmd):
-            regenerate=True
             if regenerate or target.compose_result=="":
                 self.result = ""
                 self.ports = []
