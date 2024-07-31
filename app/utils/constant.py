24dfde85271521e1a9633037c2e484463618654d
parshva10 <parshva.shah@technostacks.com>
2024-07-31 18:45:10 +0530

    add constant.py, template for sending email, update swagger.py

---
+++ app/utils/constant.py
@@ -0,0 +1,194 @@
+from django.template import Context, Template
+class EmailTemplates:
+    def __init__(self, template_name, **kwargs) -> None:
+        self.html_str = """
+        <table
+        style="font-family: 'Poppins', sans-serif"
+        role="presentation"
+        cellpadding="0"
+        cellspacing="0"
+        width="100%"
+        border="0"
+        >
+        <tbody>
+            <tr>
+            <td
+                style="
+                overflow-wrap: break-word;
+                word-break: break-word;
+                padding: 33px 55px 30px 55px;
+                font-family: 'Poppins', sans-serif;
+                "
+                align="left"
+            >
+                <div
+                style="
+                    font-size: 14px;
+                    line-height: 160%;
+                    text-align: center;
+                    word-wrap: break-word;
+                "
+                >
+                {%for line in lines%}
+                <p style="font-size: 14px; line-height: 160%">
+                    <span style="font-size: 22px; line-height: 35.2px"
+                    >{{line}}
+                    </span>
+                </p>
+                {% endfor %}
+                </div>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+        {% if button_label %}
+        <table
+        style="font-family: 'Poppins', sans-serif"
+        role="presentation"
+        cellpadding="0"
+        cellspacing="0"
+        width="100%"
+        border="0"
+        >
+        <tbody>
+            <tr>
+            <td
+                style="
+                overflow-wrap: break-word;
+                word-break: break-word;
+                padding: 10px;
+                font-family: 'Poppins', sans-serif;
+                "
+                align="left"
+            >
+                <div align="center">
+                <a
+                    href="{{button_link}}"
+                    target="_blank"
+                    class="v-button"
+                    style="
+                    box-sizing: border-box;
+                    display: inline-block;
+                    font-family: 'Poppins', sans-serif;
+                    text-decoration: none;
+                    -webkit-text-size-adjust: none;
+                    text-align: center;
+                    color: #ffffff;
+                    background-color: #ff6600;
+                    border-radius: 4px;
+                    -webkit-border-radius: 4px;
+                    -moz-border-radius: 4px;
+                    width: auto;
+                    max-width: 100%;
+                    overflow-wrap: break-word;
+                    word-break: break-word;
+                    word-wrap: break-word;
+                    mso-border-alt: none;
+                    font-size: 14px;
+                    "
+                >
+                    <span
+                    style="display: block; padding: 14px 44px 13px; line-height: 120%"
+                    ><span style="font-size: 16px; line-height: 19.2px"
+                        ><strong
+                        ><span
+                            style="
+                            line-height: 19.2px;
+                            font-size: 16px;
+                            text-transform: uppercase;
+                            "
+                            >{{button_label}}</span
+                        ></strong
+                        >
+                    </span>
+                    </span>
+                </a>
+                </div>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+        {% endif %}
+        """
+        self.templates = {
+            "forgot_password":self.forgot_password,
+            "reset_password": self.reset_password,
+            "verify_email": self.verify_email,
+            "resend_reset_password": self.resend_reset_password
+        }
+        self.result = self.templates[template_name](**kwargs)
+        
+
+    def forgot_password(self,**kwargs):
+        user = kwargs.get('user')
+        button_label = "Reset Password"
+        button_link = kwargs.get('button_links', '#')
+
+        lines = [
+            f"Hello {user.name},",
+            "We received a request to reset your password. If you did not make this request, please ignore this email.",
+            "To reset your password, please click on the link below:"
+        ]
+
+        context = {
+            'lines': lines,
+            'button_label': button_label,
+            'button_link': button_link[0],
+        }
+        template = Template(self.html_str)
+        html_content = template.render(Context(context))
+        return html_content
+    
+    def reset_password(self,**kwargs):
+        user = kwargs.get('user')
+
+        lines = [
+            f"Hello {user.name},",
+            "Your password has been successfully reset. Please use your new password to login.",
+            "If you did not initiate this password reset, please contact our support team immediately."
+        ]
+        context = {
+            'lines': lines,
+        }
+        template = Template(self.html_str)
+        html_content = template.render(Context(context))
+        return html_content
+
+
+    def verify_email(self, **kwargs):
+        user = kwargs.get('user')
+        button_label = "VERIFY YOUR EMAIL"
+        button_link = kwargs.get('button_links', '#')
+
+        lines = [
+            f"Hello {user.name},",
+            "You're almost ready to get started. Please click on the button below to verify your email address.",
+        ]
+        context = {
+            'lines': lines,
+            'button_label': button_label,
+            'button_link': button_link[0],
+        }
+        template = Template(self.html_str)
+        html_content = template.render(Context(context))
+        return html_content
+    
+    def resend_reset_password(self, **kwargs):
+        user = kwargs.get('user')
+        button_label = "Reset Password"
+        button_link = kwargs.get('button_links', '#')
+
+        lines = [
+            f"Hello {user.name},",
+            "We received a request to reset your password. If you did not make this request, please ignore this email.",
+            "To reset your password, please click on the link below:"
+        ]
+
+        context = {
+            'lines': lines,
+            'button_label': button_label,
+            'button_link': button_link[0],
+        }
+        template = Template(self.html_str)
+        html_content = template.render(Context(context))
+        return html_content
