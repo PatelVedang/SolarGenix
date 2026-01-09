776ed96ee97a878460ba476724bc69996bed7f2e
vedang-patel-techno <vedang.patel@technostacks.in>
2026-01-09 11:24:00 +0530

    Revert "fix : imports in init in core->models"
    This reverts commit d0ac7ca2a2b4a42502bcd1757c83e50e6fa5317a.

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,11 +1,11 @@
 # ruff: noqa: F403
-# ADD NEW IMPORT FOR MODEL HERE
-from .TestApp1111111 import *
 
-from .todos import *
+# ADD NEW IMPORT FOR MODEL HERE
+from .todos import Todos
 from .users import *
 from .auth_api import *
 
 __all__ = [
     # ADD NEW MODEL HERE
+    'Todos',
 ]
