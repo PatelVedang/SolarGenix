d0ac7ca2a2b4a42502bcd1757c83e50e6fa5317a
vedang-patel-techno <vedang.patel@technostacks.in>
2026-01-09 11:21:00 +0530

    fix : imports in init in core->models

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,11 +1,11 @@
 # ruff: noqa: F403
-
 # ADD NEW IMPORT FOR MODEL HERE
-from .todos import Todos
+from .TestApp1111111 import *
+
+from .todos import *
 from .users import *
 from .auth_api import *
 
 __all__ = [
     # ADD NEW MODEL HERE
-    'Todos',
 ]
